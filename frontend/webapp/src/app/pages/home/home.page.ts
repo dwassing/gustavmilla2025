86ded56ca8ced4710f5998c4f6f12b776873540f
dwassing <d.wassing@gmail.com>
2024-08-17 16:29:34 +0200

    Very basic logic for home and login pages

--- frontend/webapp/src/app/pages/home/home.page.ts
+++ frontend/webapp/src/app/pages/home/home.page.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { LoginPage } from '../login/login.page';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [LoginPage],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss'
 })
 export class HomePage {
+  constructor(
+    private router: Router
+  ) {
+
+  }
+
+  goToLoginPage(): void {
+    this.router.navigate([`login`])
+  }
 
 }
--- frontend/webapp/src/app/pages/login/login.page.ts
+++ frontend/webapp/src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../../services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrl: './login.page.scss'
 })
 export class LoginPage {
+  constructor(
+    private loginService: LoginService,
+    private router: Router
+  ){
 
+  }
+
+  loginUser(){
+    this.loginService.loginUser('danwa', 'danwa').subscribe(res => {
+      if (res.token){
+        localStorage.setItem('firstname', res.first_name);
+        localStorage.setItem('lastname', res.last_name);
+        localStorage.setItem('token', res.token);
+        this.router.navigate([`profile`]);
+      }
+    })
+
+  }
 }
