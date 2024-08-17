2dc50d9e196c094fce5d06ebd0dbb2f804136453
dwassing <d.wassing@gmail.com>
2024-08-17 16:30:15 +0200

    Added proper routing with auth guard

--- frontend/webapp/src/app/app.component.ts
+++ frontend/webapp/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomePage } from "./home/home.page";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomePage],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- frontend/webapp/src/app/app.routes.ts
+++ frontend/webapp/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './pages/login/login.page';
+import { HomePage } from './pages/home/home.page';
+import { ProfilePage } from './pages/profile/profile.page';
+import { AuthGuard } from './auth/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePage},
+  { path: 'login', component: LoginPage },
+  { path: 'profile', component: ProfilePage, canActivate: [AuthGuard] },
+];
