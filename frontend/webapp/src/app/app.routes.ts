a2f172c50486c96f527e700fc69de55cf4a658ab
dwassing <d.wassing@gmail.com>
2024-08-18 17:06:40 +0200

    Added more routes

--- frontend/webapp/src/app/app.routes.ts
+++ frontend/webapp/src/app/app.routes.ts
@@ -3,9 +3,13 @@ import { LoginPage } from './pages/login/login.page';
 import { HomePage } from './pages/home/home.page';
 import { ProfilePage } from './pages/profile/profile.page';
 import { AuthGuard } from './auth/auth.guard';
+import { InfoPage } from './pages/info/info.page';
+import { SchedulePage } from './pages/schedule/schedule.page';
 
 export const routes: Routes = [
   { path: '', component: HomePage},
   { path: 'login', component: LoginPage },
+  { path: 'info', component: InfoPage },
+  { path: 'schedule', component: SchedulePage },
   { path: 'profile', component: ProfilePage, canActivate: [AuthGuard] },
 ];
