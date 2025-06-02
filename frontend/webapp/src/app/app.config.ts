e36bd1a0bb13730244ee9e595bdfd9dbc990d6b5
dwassing <d.wassing@gmail.com>
2025-06-02 21:32:47 +0200

    First draft of owl carousel

--- frontend/webapp/src/app/app.config.ts
+++ frontend/webapp/src/app/app.config.ts
@@ -4,9 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withInterceptorsFromDi()),
+  providers: [
+    provideRouter(routes), 
+    provideHttpClient(withInterceptorsFromDi()),
+    provideAnimations(), // ✅ Add this line
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
--- frontend/webapp/src/app/pages/home/home.page.ts
+++ frontend/webapp/src/app/pages/home/home.page.ts
@@ -2,17 +2,46 @@ import { Component } from '@angular/core';
 import { LoginPage } from '../login/login.page';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LoginPage],
+  imports: [LoginPage, CarouselModule],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss'
 })
 export class HomePage {
   message: string | null = null;
   isLoggedIn: boolean = false;
+  images = [
+    'assets/images/IMG_2.jpg',
+    'assets/images/IMG_3.JPG',
+    'assets/images/IMG_4.jpg',
+    'assets/images/IMG_5.JPG',
+    'assets/images/IMG_6.JPG',
+    'assets/images/IMG_7.jpg',
+    'assets/images/IMG_8.jpg',
+    'assets/images/IMG_9.jpg',
+    'assets/images/IMG_10.jpg',
+    'assets/images/IMG_11.jpg',
+    'assets/images/IMG_12.jpg',
+    'assets/images/IMG_13.jpg',
+  ];
+  customOptions = {
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    responsive: {
+      0: { items: 1 },
+      600: { items: 1 },
+      1000: { items: 2 }
+    }
+  };
+
   constructor(
     private router: Router, private authService: AuthService
   ) {
